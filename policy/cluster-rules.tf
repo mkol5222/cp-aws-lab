ee911fbf7a8f6c0082b8bf434d5dd1d448f4300c
mkol5222 <mkoldov@gmail.com>
2025-06-19 13:34:20 +0000

    .

--- policy/cluster-rules.tf
+++ policy/cluster-rules.tf
@@ -77,3 +77,34 @@ resource "checkpoint_management_access_section" "ingress" {
   position = {above = checkpoint_management_access_section.misc.id }
   layer = "${checkpoint_management_package.cluster.name} Network"
 }
+
+resource "checkpoint_management_access_rule" "from_codespace" {
+  layer = "${checkpoint_management_package.cluster.name} Network"
+  
+  position = { below = checkpoint_management_access_section.ingress.id }
+  name     = "from Codespace"
+
+  source = [checkpoint_management_host.codespace.id]
+
+  enabled = true
+
+  destination        = ["Any"]
+  destination_negate = false
+
+  service        = ["Any"]
+  service_negate = false
+
+  action = "Accept"
+  action_settings = {
+    enable_identity_captive_portal = false
+  }
+
+  track = {
+    accounting              = false
+    alert                   = "none"
+    enable_firewall_session = true
+    per_connection          = true
+    per_session             = true
+    type                    = "Log"
+  }
+}
