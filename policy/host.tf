26b2109adb0abdc0e52a908e64230dae73029cca
mkol5222 <mkoldov@gmail.com>
2025-06-20 14:13:33 +0000

    host ip demo

--- policy/host.tf
+++ policy/host.tf
@@ -1,6 +1,14 @@
 resource "checkpoint_management_host" "example" {
   name         = "tf-host"
-  ipv4_address = "192.0.2.1"
+  ipv4_address = ""
+  color        = "red"
+  tags         = ["tag1", "tag2", "madeByTf"]
+  comments     = "This is a new host"
+}
+
+resource "checkpoint_management_host" "example2" {
+  name         = "tf-host2"
+  ipv4_address = "192.0.2.2"
   color        = "red"
   tags         = ["tag1", "tag2", "madeByTf"]
   comments     = "This is a new host"
